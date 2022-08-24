8075f9d4864f8dffc5a04c1df55ef25ae9a944c1
linnea <linnea.albertsson@gmail.com>
2022-08-24 10:08:10 +0200

    Set up basics with vite

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const persons = [
+  {
+    fullname: 'Linnea Albertsson',
+    image: '',
+    skills: [],
+    contact: { email: 'linnea.albertsson.@gmail.com', phone: 0701234567 },
+    presentation:
+      'Lorem ipsum dolor sit amet. Et aliquid consequatur eos facere corrupti ut repellendus quaerat est rerum voluptas At beatae alias. Et enim internos in eaque eaque et sint voluptas.',
+  },
+  {
+    fullname: 'Linnea Albertsson',
+    image: '',
+    skills: [],
+    contact: { email: 'linnea.albertsson.@gmail.com', phone: 0701234567 },
+    presentation:
+      'Lorem ipsum dolor sit amet. Et aliquid consequatur eos facere corrupti ut repellendus quaerat est rerum voluptas At beatae alias. Et enim internos in eaque eaque et sint voluptas.',
+  },
+  {
+    fullname: 'Linnea Albertsson',
+    image: '',
+    skills: [],
+    contact: { email: 'linnea.albertsson.@gmail.com', phone: 0701234567 },
+    presentation:
+      'Lorem ipsum dolor sit amet. Et aliquid consequatur eos facere corrupti ut repellendus quaerat est rerum voluptas At beatae alias. Et enim internos in eaque eaque et sint voluptas.',
+  },
+  {
+    fullname: 'Linnea Albertsson',
+    image: '',
+    skills: [],
+    contact: { email: 'linnea.albertsson.@gmail.com', phone: 0701234567 },
+    presentation:
+      'Lorem ipsum dolor sit amet. Et aliquid consequatur eos facere corrupti ut repellendus quaerat est rerum voluptas At beatae alias. Et enim internos in eaque eaque et sint voluptas.',
+  },
+];
