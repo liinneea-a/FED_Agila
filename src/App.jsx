b583a00755e377deaa18713741bfb6e0496d698a
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-08-26 15:00:59 +0200

    create-getintouch-section Fixes #18

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
+import ContactSection from './components/ContactSection'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-     <h1>Hejsan first test.</h1>
+      <ContactSection />
     </div>
   )
 }
