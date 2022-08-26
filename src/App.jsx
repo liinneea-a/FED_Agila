1cb3bf35c1a8cb14d505d7559b559c17511cbf15
linnea <linnea.albertsson@gmail.com>
2022-08-26 14:38:06 +0200

    create footer component #11

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import SubHero from './subHero';
+import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-    <SubHero/>
+    {/* <SubHero/> */}
+    <Footer/>
     </div>
   )
 }
