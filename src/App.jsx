c88160da8d0dc8424ec1bd5a2a9732d147738f81
PettoDavida <maximilian.widman@gmail.com>
2022-08-29 16:08:50 +0200

    Mobile responsivity for header and hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import './styles/header.css'
-import './styles/index.css'
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import "./styles/header.css";
+import "./styles/index.css";
 
 function App() {
-
   return (
     <div className="App">
-     <Header/>
-     <Hero/>
+      <Header />
+      <Hero />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,14 +1,16 @@
-
 function Hero() {
-
   return (
     <div className="hero">
-        <div className="heroContent">
+      <div className="heroContent">
         <h1>We make creative design!</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat expedita omnis ad! Aliquam beatae quae quisquam quidem soluta iste laudantium?</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          expedita omnis ad! Aliquam beatae quae quisquam quidem soluta iste
+          laudantium?
+        </p>
         <button>See more</button>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
-export default Hero
+export default Hero;
