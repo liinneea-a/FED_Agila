a63e40415cc1ceffcdbbbb6eecc795ca499776e4
PettoDavida <maximilian.widman@gmail.com>
2022-08-30 14:26:05 +0200

    Removed chakra, because it interferred with styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,7 @@
 import LoginIcon from "@mui/icons-material/Login";
 import MenuIcon from "@mui/icons-material/Menu";
-import { useDisclosure } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
-import {
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-} from "@chakra-ui/react";
 
 function Header() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
     <header>
       <ul className="headerList">
@@ -43,41 +30,7 @@ function Header() {
         </li>
       </ul>
       <div className="headerBurger">
-        <Button variant="ghost" onClick={onOpen}>
-          <MenuIcon />
-        </Button>
-        <Drawer placement={"right"} onClose={onClose} isOpen={isOpen}>
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerHeader borderBottomWidth="1px">
-              <a href="/">
-                <LoginIcon />
-              </a>
-            </DrawerHeader>
-            <DrawerBody>
-              <ul className="burgerList">
-                <li>
-                  <a href="/">Home</a>
-                </li>
-                <li>
-                  <a href="/">Services</a>
-                </li>
-                <li>
-                  <a href="/">About Us</a>
-                </li>
-                <li>
-                  <a href="/">Team</a>
-                </li>
-                <li>
-                  <a href="/">Portfolio</a>
-                </li>
-                <li>
-                  <a href="/">Contact</a>
-                </li>
-              </ul>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
+        <MenuIcon />
       </div>
     </header>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/App.css";
-import { ChakraProvider } from "@chakra-ui/react";
 import "./styles/gallery.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
