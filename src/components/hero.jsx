32e2494d535a801b93d7a9a74ab2503165c8d057
PettoDavida <maximilian.widman@gmail.com>
2022-08-30 14:35:04 +0200

    fixed styling for hero h1

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <div className="hero">
       <div className="heroContent">
-        <h1>We make creative design!</h1>
+        <h1 className="heroHeading">We make creative design!</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
           expedita omnis ad! Aliquam beatae quae quisquam quidem soluta iste
