6bd93274297cbc99993155019127ced6247c4eb7
PettoDavida <maximilian.widman@gmail.com>
2022-08-30 14:50:26 +0200

    spelling error

--- src/subHero.jsx
+++ src/subHero.jsx
@@ -1,7 +1,7 @@
-import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects'
-import TuneIcon from '@mui/icons-material/Tune'
-import Headliner from './components/Headliner'
-import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
+import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
+import TuneIcon from "@mui/icons-material/Tune";
+import Headliner from "./components/Headliner";
+import ElectricBoltIcon from "@mui/icons-material/ElectricBolt";
 
 export default function SubHero() {
   return (
@@ -23,30 +23,30 @@ export default function SubHero() {
         </div>
         <div style={iconsStyle}>
           <ElectricBoltIcon style={icon} />
-          <p className="largePText">Innovative and imoressive</p>
+          <p className="largePText">Innovative and impressive</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 const iconsContainerStyle = {
-  display: 'flex',
+  display: "flex",
   flexWrap: "wrap",
-  justifyContent: "center"
-}
+  justifyContent: "center",
+};
 
 const iconsStyle = {
-  width: '15.6rem',
-  padding: '1rem',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'start',
-  alignItems: 'center',
-  fontSize: '24px',
-  textAlign: 'center',
-}
+  width: "15.6rem",
+  padding: "1rem",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "start",
+  alignItems: "center",
+  fontSize: "24px",
+  textAlign: "center",
+};
 
 const icon = {
-  fontSize: '150px',
-}
+  fontSize: "150px",
+};
