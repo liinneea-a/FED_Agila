f9c3d972d2b837221799ee0efc0f2886ff6df87d
linnea <linnea.albertsson@gmail.com>
2022-08-31 16:22:40 +0200

    Added styling

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -1,35 +1,73 @@
+import { useParams } from 'react-router-dom';
+import { team } from './data';
+
 export default function ProfilePage() {
+  let { id } = useParams();
+  let person;
+
+  for (let member of team) {
+    if (member.firstname === id) {
+      person = member;
+    }
+  }
+
   return (
     <div className="profilePageContainer">
-
       <div className="sidebar">
-        <div className="imgContainer"></div>
+        <img className="imgContainer" src={person.image}/>
         <div className="aboutMeContainer">
           <p className="team-card-name profilePageHeadline">ABOUT ME</p>
+          <p>{person.presentation}</p>
+        </div>
+        <div>
+          <p className="team-card-name">CONTACT</p>
           <p>
-            I'm 25 years old and currently living in Gothenburg, Sweden. I am
-            passionate about creating and finding new cool ways to do so. For a
-            long time, my base has been on stage or in front of a camera, acting
-            and singing (I'm a huge fan of musicals). But have been doing that
-            for a few years I wanted to try something new.
+            {person.contact.email} <br /> 070 1234567
           </p>
         </div>
-        <div className="contactTextContainer">
-          <p className="team-card-name">CONTACT</p>
-          <p>mail@mail.com <br/> 070 1234567</p>
-        </div>
       </div>
-      
+
       <div className="profileInfo">
-        <h2>LINNEA ALBERTSSON</h2>
-        <p>Front-end Developer</p>
-        <div className="workExperience">
-            <p>WORK EXPERIENCE</p>
-            <hr/>
+        <div className="headline">
+          <h2 className="name">{person.firstname} {person.lastname}</h2>
+          <p>{person.role}</p>
         </div>
 
+        <div className="container">
+          <p className="team-card-name">WORK EXPERIENCE</p>
+          <hr />
+          <div>
+            <span className='fatText'>{person.role},</span>
+            <span> Företag</span>
+          </div>
+          <ul>
+            <li>Sed consequatur quis ut maiores dicta ut delectus dolores.</li>
+            <li>
+              Eum eius deserunt est praesentium suscipit sed neque obcaecati eos
+              minus consequatur sit eveniet corporis vel voluptatum pariatur. Id
+              deserunt fuga nam neque sint eos rerum ipsam.
+            </li>
+          </ul>
+        </div>
+        <div className="container">
+          <p className="team-card-name">SKILLS</p>
+          <hr />
+        
+          <span>LOGO </span>
+          <span>LOGO </span>
+          <span>LOGO </span>
+          <span>LOGO </span>
+        </div>
+        <div className="container">
+          <p className="team-card-name">EDUCATION</p>
+          <hr />
+          <p>
+            Medieinstitutet, <span>{person.role}</span>
+            <br />
+            <span>September 2021 - Maj 2023</span>
+          </p>
+        </div>
       </div>
-
     </div>
   );
 }
--- src/TeamCard.jsx
+++ src/TeamCard.jsx
@@ -1,15 +1,18 @@
 import { team } from "./data";
+import {Link} from 'react-router-dom';
 
 function TeamCard() {
   return (
     <>
+
       {team &&
         team.map((team) => (
+          <Link to={`/profile/${team.firstname}`} > 
           <li
             className="grid-item"
-            key={team.id}
-            onClick={() => <link to="`/team/${team.slug}`" />}
+            key={team.firstname}
           >
+            
             <img src={team.image}/>
             <h2 className="team-card-name">
               {team.firstname}{' '}
@@ -17,6 +20,7 @@ function TeamCard() {
             </h2>
             <h3 className="team-card-role">{team.role}</h3>
           </li>
+          </Link>
         ))}
     </>
   );
