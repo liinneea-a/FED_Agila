8fa76ef488a19abac5631fb368ace46df468bad9
PettoDavida <maximilian.widman@gmail.com>
2022-08-31 14:51:10 +0200

    added burger menu with react burger menu

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import mapImg from '../assets/images/map.jpg'
-import ContactForm from './ContactForm'
-import Headliner from './Headliner'
+import React from "react";
+import mapImg from "../assets/images/map.jpg";
+import ContactForm from "./ContactForm";
+import Headliner from "./Headliner";
 
 const ContactSection = () => {
   return (
-    <div className="section">
+    <div className="section" id="contact">
       <Headliner
         title="Get In Touch"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus "
       />
-      <ContactForm/>
+      <ContactForm />
       <div>
         <img src={mapImg} alt="a map showing location of company" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactSection
+export default ContactSection;
