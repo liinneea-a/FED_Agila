72bceb277a4c384f348ecf1661d5f6b37e5e6db1
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-08-31 16:28:25 +0200

    toggable skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,22 @@
 import Headliner from './Headliner'
+import { skills } from '../data'
+import { useState } from 'react'
 
 const Skills = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="section">
-      <Headliner title="test" text="test" />
+      <Headliner
+        title="Tech Stack"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus "
+      />
+      <div className="skill-grid">
+        <div className="skill-grid-item" onClick={() => setOpen(!open)}>
+          <h3>html</h3>
+          <p className={open ? 'show' : 'hide'}>description</p>
+        </div>
+      </div>
     </div>
   )
 }
