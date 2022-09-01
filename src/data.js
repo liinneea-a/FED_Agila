c33b1505c8814d6939ce2798c03053bcd9495eba
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-09-01 11:42:29 +0200

    Connect skills to employees Fixes #57

--- src/data.js
+++ src/data.js
@@ -21,6 +21,10 @@ import socketio from '../images/Socket-io.png';
 import svelte from '../images/Svelte_Logo.png';
 import typescript from '../images/typescript-logo.png';
 import vue from '../images/Vue-logo.png';
+import css from '../images/css-logo.png';
+import csharp from '../images/csharp-logo.png'
+import wordpress from '../images/wordpress-logo.png'
+import next from '../images/next-js-logo.png'
 
 export const skills = [
   {
@@ -28,109 +32,121 @@ export const skills = [
     description:
       'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.',
     image: html
-    },
+  },
   {
     name: 'CSS',
     description:
       'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.',
     image: html
-    },
+  },
   {
     name: 'React',
     description:
       'React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies. React can be used as a base in the development of single-page, mobile, or server-rendered applications with frameworks like Next.js. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing, as well as certain client-side functionality.',
     image: react
-    },
+  },
   {
     name: 'TypeScript',
     description:
       'TypeScript is a free and open source programming language developed and maintained by Microsoft. It is a strict syntactical superset of JavaScript and adds optional static typing to the language. It is designed for the development of large applications and transpiles to JavaScript. As it is a superset of JavaScript, existing JavaScript programs are also valid TypeScript programs.  ',
     image: typescript
-    },
+  },
   {
     name: 'Javascript',
     description:
       "JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users' devices. JavaScript is a high-level, often just-in-time compiled language that conforms to the ECMAScript standard. It has dynamic typing, prototype-based object-orientation, and first-class functions. It is multi-paradigm, supporting event-driven, functional, and imperative programming styles. It has application programming interfaces (APIs) for working with text, dates, regular expressions, standard data structures, and the Document Object Model (DOM).",
     image: javascript
-    },
+  },
   {
     name: 'Figma',
     description:
       'Figma is a free, online UI tool to create, collaborate, prototype, and handoff.',
     image: figma
-    },
+  },
   {
     name: 'p5',
     description:
       'p5.js is a JavaScript library for creative coding, with a focus on making coding accessible and inclusive for artists, designers, educators, beginners, and anyone else.',
     image: p5js
-    },
+  },
   {
     name: 'MongoDB',
     description:
       'MongoDB is a source-available cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with optional schemas. MongoDB is developed by MongoDB Inc. and licensed under the Server Side Public License (SSPL) which is deemed non-free by several distributions.',
     image: mongodb
-    },
+  },
   {
     name: 'Vue',
     description:
       'Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications. It was created by Evan You, and is maintained by him and the rest of the active core team members.',
     image: vue
-    },
+  },
   {
     name: 'C#',
     description:
       'C# is a general-purpose, multi-paradigm programming language. C# encompasses static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.',
     image: vue
-    },
+  },
   {
     name: 'WordPress',
     description:
       'WordPress is a free and open-source content management system written in PHP and paired with a MySQL or MariaDB database with supported HTTPS. Features include a plugin architecture and a template system, referred to within WordPress as Themes.',
-    image: vue
-    },
+    image: wordpress
+  },
   {
     name: 'Adobe Photoshop',
     description:
       'Photoshop is an image creation, graphic design and photo editing software developed by Adobe. The software provides many image editing features for pixel-based images, raster graphics and vector graphics.',
     image: photoshop
-    },
+  },
   {
     name: 'Next',
     description:
       'Next.js is an open-source web development framework built on top of Node.js enabling React-based web applications functionalities such as server-side rendering and generating static websites.',
-    image: vue
-    },
+    image: next
+  },
   {
     name: 'Svelte',
     description:
       'Svelte is a free and open-source front end compiler created by Rich Harris and maintained by the Svelte core team members. Svelte is not a monolithic JavaScript library imported by applications: instead, Svelte compiles HTML templates to specialized code that manipulates the DOM directly, which may reduce the size of transferred files and give better client performance; application code is also processed by the compiler, inserting calls to automatically recompute data and re-render UI elements when the data they depend on is modified. This also avoids the overhead associated with runtime intermediate representations, such as virtual DOM, unlike traditional frameworks (such as React and Vue) which carry out the bulk of their work at runtime, i.e in the browser. The compiler itself is written in TypeScript. Its source code is licensed under MIT License and hosted on GitHub.',
     image: svelte
-    },
+  },
   {
     name: 'Express',
     description:
       'Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.',
     image: express
-    },
+  },
   {
     name: 'NodeJS',
     description:
       "Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on a JavaScript Engine (i.e. V8 engine) and executes JavaScript code outside a web browser, which was designed to build scalable network applications. Node.js lets developers use JavaScript to write command line tools and for server-side scripting—running scripts server-side to produce dynamic web page content before the page is sent to the user's web browser. Consequently, Node.js represents a 'JavaScript everywhere' paradigm, unifying web-application development around a single programming language, rather than different languages for server-side and client-side scripts.",
     image: nodejs
-    },
+  },
   {
     name: 'Mongoose',
     description:
       'Mongoose is a JavaScript object-oriented programming library that creates a connection between MongoDB and the Node.js JavaScript runtime environment. Mongoose provides a straight-forward, schema-based solution to model your application data. It includes built-in type casting, validation, query building, business logic hooks and more, out of the box.',
     image: mongoose
-    },
+  },
   {
     name: 'SocketIO',
     description:
       'Socket.IO is a library that enables low-latency, bidirectional and event-based communication between a client and a server. It is built on top of the WebSocket protocol and provides additional guarantees like fallback to HTTP long-polling or automatic reconnection.',
     image: socketio
-    },
+  },
+  {
+    name: 'Csharp',
+    description:
+      '',
+    image: csharp
+  },
+  {
+    name: 'CSS',
+    description:
+      '',
+    image: css
+  }
 ];
 
 
@@ -156,7 +172,6 @@ export const team = [
       socketio,
       typescript,
       vue,
-
     ],
     contact: { email: 'linnea.albertsson@gmail.com' },
     presentation:
@@ -168,18 +183,18 @@ export const team = [
     image: sabinaImage,
     role: 'Front-end developer',
     skills: [
-      skills.html,
-      skills.css,
-      skills.csharp,
-      skills.React,
-      skills.javaScript,
-      skills.Typescript,
-      skills.figma,
-      skills.p5,
-      skills.mongodb,
-      skills.vue,
-      skills.wordpress,
-      skills.photoshop,
+      css,
+      csharp,
+      html,
+      react,
+      javascript,
+      typescript,
+      figma,
+      p5js,
+      mongodb,
+      vue,
+      wordpress,
+      photoshop,
     ],
     contact: { email: 'sabinaander@hotmail.com' },
     presentation:
@@ -191,16 +206,16 @@ export const team = [
     image: tomasImage,
     role: 'Front-end developer',
     skills: [
-      skills.html,
-      skills.css,
-      skills.javaScript,
-      skills.typescript,
-      skills.figma,
-      skills.react,
-      skills.next,
-      skills.p5,
-      skills.mongodb,
-      skills.svelte,
+      html,
+      css,
+      javascript,
+      typescript,
+      figma,
+      react,
+      next,
+      p5js,
+      mongodb,
+      svelte,
     ],
     contact: { email: 'tomasfridekrans@outlook.com' },
     presentation:
@@ -212,14 +227,14 @@ export const team = [
     image: maximilianImage,
     role: 'Front-end developer',
     skills: [
-      skills.html,
-      skills.css,
-      skills.javaScript,
-      skills.figma,
-      skills.p5,
-      skills.mongodb,
-      skills.react,
-      skills.typescript,
+      html,
+      css,
+      javascript,
+      figma,
+      p5js,
+      mongodb,
+      react,
+      typescript,
     ],
     contact: { email: 'maximilian.widman@gmail.com' },
     presentation:
@@ -231,13 +246,13 @@ export const team = [
     image: kawanImage,
     role: 'Front-end developer',
     skills: [
-      skills.html,
-      skills.css,
-      skills.javaScript,
-      skills.typescript,
-      skills.figma,
-      skills.react,
-      skills.mongodb,
+      html,
+      css,
+      javascript,
+      typescript,
+      figma,
+      react,
+      mongodb,
     ],
     contact: { email: 'hello@kawium.dev' },
     presentation:
