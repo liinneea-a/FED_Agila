c06a06bb5a3bd13f0ce990d1363fa73fa718464c
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-09-01 14:53:46 +0200

    minor fixes due to old version

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-
-import Header from "./components/header";
-import Hero from "./components/hero";
-import SubHero from "./subHero";
-import ContactSection from "./components/ContactSection";
-import Footer from "./Footer";
-import TeamGallery from "./TeamGallery";
-import "./styles/header.css";
-import "./styles/index.css";
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
-import Hero from './components/hero';
-import SubHero from './subHero';
-import ContactSection from './components/ContactSection';
 import Footer from './Footer';
-import TeamGallery from './TeamGallery';
 import ProfilePage from './ProfilePage';
 import StartPage from './StartPage';
 import './styles/header.css';
@@ -24,12 +11,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Hero />
-      <SubHero />
-      <TeamGallery />
-      <ContactSection />
-      <AboutUs />
-      <Footer />
       <BrowserRouter>
         <Routes>
           <Route path="profile/:id" element={<ProfilePage />}/>
