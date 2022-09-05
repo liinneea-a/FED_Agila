ca33bee86a67cb545b4abbe0043f91e29ce80835
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-09-05 18:33:03 +0200

    key buggfix

--- src/TeamCard.jsx
+++ src/TeamCard.jsx
@@ -1,13 +1,13 @@
-import { team } from './data';
-import { Link } from 'react-router-dom';
+import { team } from './data'
+import { Link } from 'react-router-dom'
 
 function TeamCard() {
   return (
     <>
       {team &&
         team.map((team) => (
-          <Link to={`/profile/${team.firstname}`}>
-            <li className="grid-item" key={team.firstname}>
+          <Link to={`/profile/${team.firstname}`} key={team.firstname}>
+            <li className="grid-item">
               <img src={team.image} />
               <h2 className="team-card-name">
                 {team.firstname} {team.lastname}
@@ -17,7 +17,7 @@ function TeamCard() {
           </Link>
         ))}
     </>
-  );
+  )
 }
 
-export default TeamCard;
+export default TeamCard
