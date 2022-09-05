b07d4f398a4a4ea60ee3072edcac33b21baaea0a
linnea <linnea.albertsson@gmail.com>
2022-09-05 16:13:20 +0200

    Gör profilsidan responsiv Fixes #70

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/header';
-import Hero from './components/hero';
-import SubHero from './subHero';
-import ContactSection from './components/ContactSection';
-import Footer from './Footer';
-import TeamGallery from './TeamGallery';
+
 import ProfilePage from './ProfilePage';
 import StartPage from './StartPage';
 import './styles/header.css';
@@ -16,14 +10,12 @@ import './styles/index.css';
 function App() {
   return (
     <div className="App">
-      <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="profile/:id" element={<ProfilePage />}/>
-          <Route path="/" element={<StartPage/>} />
+          <Route path="profile/:id" element={<ProfilePage />} />
+          <Route path="/" element={<StartPage />} />
         </Routes>
       </BrowserRouter>
-      <Footer/>
     </div>
   );
 }
