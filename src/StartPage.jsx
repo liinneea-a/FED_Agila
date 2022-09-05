ebc347ee5572a284d28d46e08dbdbb7d32840cd9
linnea <linnea.albertsson@gmail.com>
2022-09-05 15:29:00 +0200

    Moved header and footer to startpage

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -3,16 +3,19 @@ import SubHero from './subHero';
 import ContactSection from './components/ContactSection';
 import TeamGallery from './TeamGallery';
 import AboutUs from './components/AboutUs';
+import Header from './components/header';
+import Footer from './Footer';
 
-
-export default function StartPage(){
-    return(
-        <>
-        <Hero /> 
-        <SubHero />
-        <TeamGallery />
-        <ContactSection />
-        <AboutUs />
-        </>
-    )
+export default function StartPage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <SubHero />
+      <TeamGallery />
+      <ContactSection />
+      <AboutUs />
+      <Footer />
+    </>
+  );
 }
